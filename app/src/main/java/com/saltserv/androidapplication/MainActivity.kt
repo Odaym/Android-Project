4db42f9bb96f016c04ef7ada9279775714882fac
Oday <oday@saltserv.com>
2024-01-12 21:44:52 +0100

    Adds add

--- app/src/main/java/com/saltserv/androidapplication/MainActivity.kt
+++ app/src/main/java/com/saltserv/androidapplication/MainActivity.kt
@@ -6,10 +6,7 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import com.saltserv.androidapplication.ui.theme.AndroidApplicationTheme
 
 class MainActivity : ComponentActivity() {
@@ -26,4 +23,6 @@ class MainActivity : ComponentActivity() {
             }
         }
     }
+
+    private fun add(num1: Int, num2: Int) = num1 + num2
 }
